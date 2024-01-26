b389be3e2ac67e20c3dfd56f68a26e55a20fd2f1
GiordanoDiego <diegomgiordano96@gmail.com>
2024-01-26 12:54:49 +0100

    little fix, completed

--- src/store.js
+++ src/store.js
@@ -6,6 +6,8 @@ export const store = reactive({
     
     apiSearchST: "https://api.themoviedb.org/3/search/tv?api_key=d2932469f928fbbfb148b7bb30e09306&language=it_IT&query=",
 
+    apiActor: "https://api.themoviedb.org/3/movie/19995/credits?api_key=d2932469f928fbbfb148b7bb30e09306",
+
     urlFlag: "https://flagsapi.com/film.original_language.toUpperCase()/flat/64.png",
 
     //ARRAY-API
